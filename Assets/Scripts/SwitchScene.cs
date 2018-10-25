438b0008f1ed5eecd6519771d3a4102da7d63f2b
jphearn2 <johearn@siue.edu>
2018-10-25 17:21:45 -0500

    first test

---
+++ Assets/Scripts/SwitchScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class SwitchScene : MonoBehaviour {
+
+    
+    public void ChangeScene(string scene)
+    {
+        Application.LoadLevel(scene);
+    }
+
+    public void BackToStore()
+    {
+        Application.LoadLevel("Storefront");
+    }
+}
